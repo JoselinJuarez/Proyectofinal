bfb5946b7ae89e171372754fca374a49f964825d
jhovany6 <quispe.chiri.jhovany.6.2@gmail.com>
2024-11-02 17:35:50 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,64 @@
 <?php
 
+use App\Http\Controllers\ClientesController;
+use App\Http\Controllers\objetosController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\clientesController;
-Route::get('/', function () {
-    return view('login');
-});
+use App\Http\Controllers\PrestamosController;
+use App\Http\Controllers\LoginController;
+//Route::get('/', function () {
+   // return view('login');
+//});
 
-Route::get('/home', function () {
-    return view('home');
-});
+//Route::get('/home', function () {
+    //return view('home');
+//});
+Route::get('/home',[LoginController::class,'home'])->name('home');
 Route::get('/clientes', function () {
     return view('clientes');
 
 });
+////////cliente controller
+//cliente ver
+Route::get('/clientes',[ClientesController::class,'indexclient'])->name('clientes');
+Route::post('/clientes-insertar',[ClientesController::class,'createclient'])->name('clientes.create');
+Route::get('/client', function () {
+    return view('client');
+ });
+////
+/////
+///
+//
 
-Route::get('/clientes/nuevo', function () {
-    return view('formularios/clientesform');
-
-});
-
-Route::post('/clientes/guardar', [clientesController::class, 'guardarCliente']);
-
-
+//Route::get("/home1", [LoginController::class, 'function'])->name('prestamos');
+Route::get("/", [LoginController::class, 'showLoginForm'])->name('login');
+Route::post("/login", [LoginController::class, 'function'])->name('login.enviar');
 Route::get('/usuarios', function () {
     return view('usuarios');
 });
-Route::get('/prestamos', function () {
-    return view('prestamos');
+Route::get("/prestamos", [PrestamosController::class, 'index'])->name('prestamos');
+//dlete product
+Route::get("/prestamos/eliminar-{id}", [PrestamosController::class, 'delete'])->name('prestamos.delete');
+//Route::get("/prestamos", [PrestamosController::class, 'index'])->name('prestamos');
+//anadir product
+Route::post("/prestamos-insertar", [PrestamosController::class, 'create'])->name('prestamos.create');
+//Route::post("/prestamos-insertar", [PrestamosController::class, 'create'])->name('prestamos.create');
+//Route::get("/prestamos-update", [PrestamosController::class, 'update'])->name('prestamos.update');
+
+//insertar
+Route::get('/insertar', function () {
+   return view('prestamosinsertar');
 });
-Route::get('/garantias', function () {
-    return view('garantias');
-});
+//
+
+Route::get('/editar-{id}',[PrestamosController::class,'edit'])->name('editar.prestamos');
+//Route::get('/editarprestamos', function () {
+ ////   return view('editarprestamos');
+//});
+
+Route::get("/garantias",[objetosController::class,'indexgarantia'])->name('garantias');
+//Route::get('/garantias', function () {
+ //   return view('garantias');
+//});
 Route::get('/pagos', function () {
     return view('pagos');
 });
